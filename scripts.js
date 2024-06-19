cc4ef19300280fd144fe89a8bfe1770b54ef7458
Lachlan Harris <105751727+LachlanHarrisCCGS@users.noreply.github.com>
2024-06-19 17:39:17 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,19 +6,47 @@ document.addEventListener('scroll', function() {
     elements.forEach(function(element) {
         let positionFromTop = element.getBoundingClientRect().top;
 
-        if (positionFromTop - windowHeight <= 0) {
+        if (positionFromTop - windowHeight <= -100) {
             element.classList.add('fade-visible');
         } else {
             element.classList.remove('fade-visible');
         }
     });
 });
-
-// Apply fade class to each element on load
 document.addEventListener('DOMContentLoaded', function() {
     let elements = document.querySelectorAll('.paragraph, .quote, .image');
 
     elements.forEach(function(element) {
         element.classList.add('fade');
     });
+
+    let windowHeight = window.innerHeight;
+
+    elements.forEach(function(element) {
+        let positionFromTop = element.getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+            element.classList.add('fade-visible');
+        } else {
+            element.classList.remove('fade-visible');
+        }
+    });
+
+// Title Typing animation
+
+    const title = document.getElementById('title');
+    const text = title.textContent;
+    title.textContent = ''; // Clear the current text to start typing animation
+    title.style.opacity = 1;
+
+    let index = 0;
+
+    function typeLetter() {
+        if (index < text.length) {
+            title.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeLetter, 50); // ADJUST DELAY
+        }
+    }
+    typeLetter();
 });
